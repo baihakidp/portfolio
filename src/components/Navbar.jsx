9e704349ab7fe6f324a1a288e0c69291e62c59ca
gaming <baihakidemiandraputra@gmail.com>
2023-03-28 10:39:06 +0700

    fixing drive link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -165,7 +165,7 @@ const Navbar = () => {
                 </Link>
 
                 <Link
-                  href="https://drive.google.com/file/d/1P2718S77za94pI7Hsvl_4t0SHSrjG11n/view"
+                  href="https://drive.google.com/file/d/1rDXiQ7Hn9rV8hG16dCylytWeejwAP5nW/view?usp=share_link"
                   className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300"
                   target="_blank"
                 >
